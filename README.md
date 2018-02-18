6bdc15413f1fe2513586cd9c0f198be33caf8832
Johannes Wilm <mail@johanneswilm.org>
2018-02-18 15:59:48 +0100

    0.1 release

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # django-npm-mjs
-A Django package to take care of npm.js  ES2016+ dependencies
+A Django package to use npm.js dependencies and transpile ES2015+
+
+
+Quick start
+-----------
+
+1. Add "npm_mjs" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'npm_mjs',
+    ]
+
+2. Add "static-transpile" template tags to your templates. All entry files to ES2015+ modules need to have \*.mjs endings.
+
+3. Run `./manage.py transpile`.
+
+4. Run `./manage.py runserver`. Your ES2015 modules will be served as browser compatible ES5 files.
+
+
+NPM.JS dependencies
+-----------
+
+1. Add package.json files into one or more of your apps. All package.json files will be merged.
+
+2. Import in your JS files from any of the npm modules specified in your package.json files.
+
+3. Run `./manage.py transpile`.
+
+4. Run `./manage.py runserver`.
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-npm-mjs',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='LGPL License',
+    description='A Django package to use npm.js dependencies and transpile ES2015+',
+    long_description=README,
+    url='https://github.com/fiduswriter/django-npm-mjs',
+    author='Johannes Wilm',
+    author_email='johannes@fiduswriter.org',
+    install_requires=['Django >= 1.11',
+                      'JSON_minify == 0.3.0'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
