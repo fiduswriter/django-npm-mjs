1d7420af60dd82f20e390c268e68b0229bce9f74
Johannes Wilm <mail@johanneswilm.org>
2023-01-04 23:56:10 +0100

    remove unused code

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(__file__, os.pardir)))
 
